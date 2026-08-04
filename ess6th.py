09b86794d543fcd997df3c24ec2f521ed2fb65f9
mkhakwani815-collab <mkhakwani815@gmail.com>
2026-08-04 06:01:59 -0700

    Daily Task

---
+++ ess6th.py
@@ -0,0 +1,8 @@
+fruits = ["apple", "banana", "cherry", "date", "elderberry"]
+
+#Expected Output: ['apple', 'cherry', 'date', 'elderberry', 'fig']
+
+fruits.pop(1)
+fruits.append("fig")
+
+print(fruits)
