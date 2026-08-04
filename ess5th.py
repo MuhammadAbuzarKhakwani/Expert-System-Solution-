6b6b8b637fb6398461acb52fdef80f0c9db971cd
mkhakwani815-collab <mkhakwani815@gmail.com>
2026-08-04 05:49:55 -0700

    Practiced

---
+++ ess5th.py
@@ -0,0 +1,13 @@
+num_1 = int(input("Enter 1st Number: "))
+num_2 = int(input("Enter 2nd Number: "))
+
+print("You entered ",num_1, "as 1st number and ", num_2, "as 2nd number")
+
+print(".....................")
+print("processing...........")
+
+num_1 = num_1 + num_2
+num_2 = num_1 - num_2
+num_1 = num_1 - num_2
+
+print("Now 1st number is ", num_1, "and 2nd number is ", num_2)
