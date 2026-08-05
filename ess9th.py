9fc1beb6de4cc59250fc59205af9d819cce93418
mkhakwani815-collab <mkhakwani815@gmail.com>
2026-08-05 00:05:05 -0700

    Pynative practice Questions

--- ess9th.py
+++ ess9th.py
@@ -4,5 +4,36 @@ if num%2 == 0:
     print("The given number ",num," is Even")
 else:
     print("The given number is odd")
+
+print(".................String Operations....................")
+
+name = "Muhammad Abuzar khakwani"
+
+print(name)
+print(name.upper())
+print(name.lower())
+print(len(name))
+print(name[0])
+print(name[(len(name)-1)])
+print(name[::-1])
+
+count = 0
+
+for i in range(len(name)):
+    match name[i]:
+        case "a":
+            count += 1
+        case "e":
+            count += 1
+        case "i":
+            count += 1
+        case "o":
+            count += 1
+        case "u":
+            count += 1
+
+print("The number of vowels in the given string is ",count)
     
+
+
 
