b94d99dcfcd5f9cdfe239e8a4e9bfd154bf48252
mkhakwani815-collab <mkhakwani815@gmail.com>
2026-08-05 00:57:04 -0700

    Hacker Rank practice

---
+++ DAY_2/ess12.py
@@ -0,0 +1,32 @@
+'''
+  Task
+Given an integer, , perform the following conditional actions:
+
+If  is odd, print Weird
+If  is even and in the inclusive range of 2-5 to , print Not Weird
+If  is even and in the inclusive range of 6-20 to , print Weird
+If  is even and greater than , print Not Weird
+Input Format
+
+A single line containing a positive integer, .
+
+Constraints
+
+Output Format
+
+Print Weird if the number is weird. Otherwise, print Not Weird'''
+
+num = int(input("Enter a Number: "))
+
+if num > 0:
+    if num%2 == 0:
+        if 2<= num <= 5:
+            print("Not Weird")
+        elif 6<= num <= 20:
+            print("Weird")
+        else:
+            print("Not Weird")
+    else:
+        print("Weird")
+else:
+    print("Please enter a positive number")
