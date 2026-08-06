c1fdf2b2b7c040a898fb73a3b3e3c8b71c7c979f
mkhakwani815-collab <mkhakwani815@gmail.com>
2026-08-06 02:04:48 -0700

    practice

--- DAY_3/ess17th.py
+++ DAY_3/ess17th.py
@@ -3,12 +3,18 @@ numbers = [1,2,5,3,2,5,2,11,1]
 
 unique = list(set(number))
 unique.sort(reverse = True)
-print(unique)
 
-number.extend(numbers)
 
-print(number)
 
-num = number - numbers
 
-print(num)
+
+nested_l = [[j for j in numbers] for i in range(5)]
+
+nested_table = [i*j for i in range(1,11) for j in range(2,3)]
+
+print(nested_table)
+
+nums = int(input("Enter any Number for Table: "))
+table = [nums*i for i in range(1,11)]
+
+print(table)
