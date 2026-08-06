7206da598e1a4dd20cb9f165ccc712dcffa7b860
mkhakwani815-collab <mkhakwani815@gmail.com>
2026-08-06 02:35:48 -0700

    tuple practice

---
+++ DAY_3/ess18th.py
@@ -0,0 +1,25 @@
+student = (
+    'Muhammad Abuzar',
+    21,
+    '31303-9501191-2',
+    'Software Engineering'
+)
+
+name , age , CNIC, dept = student
+
+print(student)
+
+print(name)
+print(age)
+print(CNIC)
+print(dept)
+
+def calculate(a,b):
+    sum = a + b
+
+    dif = a - b
+    return sum,dif
+
+print(calculate(7,5))
+
+
