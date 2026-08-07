b5202807576bca36964cda5c1ad37351311609b4
mkhakwani815-collab <mkhakwani815@gmail.com>
2026-08-07 04:39:22 -0700

    list frqueny counter and 2nd maximum - practice

---
+++ DAY_4/ess22nd.py
@@ -0,0 +1,24 @@
+list_a =[1,2,2,2,3,3,3,4,4,4,4,4,3,3,3,3]
+
+
+list_a = set(list_a)
+list_a = list(list_a)
+list_a.sort()
+
+dic_l = {}
+
+for i in list_a:
+    if i in dic_l:
+        dic_l[i] += 1
+    else:
+        dic_l[i] = 1
+
+print(dic_l)
+
+print(list_a[-2])
+
+
+
+
+
+
