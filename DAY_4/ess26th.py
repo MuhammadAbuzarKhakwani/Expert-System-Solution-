b389ba79c2a84aa36985af219e54a21a80b61464
mkhakwani815-collab <mkhakwani815@gmail.com>
2026-08-10 00:59:51 -0700

    Completed the management system

--- DAY_4/ess26th.py
+++ DAY_4/ess26th.py
@@ -19,21 +19,64 @@ def Add_p(D):
     quantity = int(input("Enter Quantity: "))
     D[name] = quantity
 
+def UPD_p(D):
+    Add_p(D)
+
+def del_p(D):
+    name = input("Enter Product name to Delete: ")
+    if name in D:
+        del D[name]
+    else:
+        print("This product does not exist")
+
+def search_p(D):
+    name = input("Enter product to Search: ")
+    if name in D:
+        print("Found it")
+
+def Display_p_i(D):
+    for key in D:
+        print("Products: ",key) 
+
+
 def sl_pause():
-    time.sleep(2)
+    time.sleep(1.5)
 
 
 
 
 def main():
     D = {}
-    print_menu()
+    c = True
+    while c:
+        sl_pause()
+        clear_screen()
 
+        print_menu()
+        try:
+            choice = int(input("Enter your choice: "))
+        except ValueError:
+            print("Banda ban oye!!.Please enter a value from 1-6")
+            print("")
 
+        match choice:
+            case 1:
+                Add_p(D)
+            case 2:
+                UPD_p(D)
+            case 3:
+                del_p(D)
+            case 4:
+                search_p(D)
+            case 5:
+                Display_p_i(D)
+            case 6:
+                c = False
+            case _:
+                print("Invalid Input")
+        
+            
 main()
-
-
-
 
 
 
