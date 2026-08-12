07ac5dc122e7f4d524a7d5f7ecfd56656b2547b1
mkhakwani815-collab <mkhakwani815@gmail.com>
2026-08-12 07:38:25 -0700

    Started python classes

---
+++ DAY_7/Classes.py
@@ -0,0 +1,11 @@
+class student:
+
+    def __init__(self,name,age,dept):
+        self.name = name 
+        self.age = age
+        self.dept = dept
+
+
+student1 = student("Abuzar",21,"Software Engineering")
+
+print(student1.name)
