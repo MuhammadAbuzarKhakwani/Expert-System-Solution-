f4bf3409537c0bc62285dd99d2a75c7f1f76bb6c
mkhakwani815-collab <mkhakwani815@gmail.com>
2026-08-17 01:23:35 -0700

    new things

--- Week-3/DAY_1/Encapsulation.py
+++ Week-3/DAY_1/Encapsulation.py
@@ -4,14 +4,58 @@ class Bank_account:
     
     def deposit(self, amount):
         self.__balance += amount
+        print("successful Deposit")
+        
+    def withdraw(self,amount):
+        if self.__balance < amount:
+            print("Insufficient balance")
+        else:
+            print("Balnce before transaction: ",self.__balance)
+            self.__balance -= amount
+            print("successful transaction")
+            print("Balnce after transaction: ",self.__balance)
 
     def display(self):
         print(self.__balance)
 
 
-abuzar = Bank_account(1500)
+
+data = int(input("Enter your balance: "))
+
+
+abuzar = Bank_account(data)
 
 abuzar.display()
+abuzar.deposit(500)
+abuzar.withdraw(3000)
+abuzar.withdraw(300)
 
 
+class student:
+    
+    def __init__(self, number):
+        self.__marks = number
+
+    @property
+    def mark(self):
+        return self.__marks
+    
+    @mark.setter
+    def mark(self,val):
+        if val < 100:
+            self.__marks = val
+        else:
+            print("Invalid marks")
+
+
+s1 = student(122)
+
+
+s1.mark = 53
+
+a = s1.mark
+
+
+
+print(a)
 
