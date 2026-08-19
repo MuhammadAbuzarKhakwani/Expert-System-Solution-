e76239e8162d0aef82563eeb5584ceea93d87105
mkhakwani815-collab <mkhakwani815@gmail.com>
2026-08-19 00:52:38 -0700

    Stack practice

---
+++ Week-3/DAY_2/Stack.py
@@ -0,0 +1,46 @@
+class stack:
+    items = []
+
+    def __init__(self):
+        self.items = []
+    
+    def push(self,data):
+        self.items.append(data)
+    
+    def pop(self):
+
+        if len(self.items) == 0:
+            print("Already empty")
+        
+        else:
+            return self.items.pop()
+    
+    def peek(self):
+        if len(items) == 0:
+            print("Already empty")
+        
+        else:
+            return self.items[-1]
+    
+    def display(self):
+        return self.items
+
+    def get_min(self):
+        self.items.sort(reverse = True)
+        return self.items[-1]
+    
+First = stack()
+
+First.pop()
+
+First.push(10)
+First.push(20)
+First.push(30)
+First.push(40)
+First.push(50)
+
+First.pop()
+
+print(First.display())
+
+print(First.get_min())
