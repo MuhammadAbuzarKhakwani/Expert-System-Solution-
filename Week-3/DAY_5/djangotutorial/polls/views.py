f563e0bbd38bd2c5da661f7574eff103b8191644
mkhakwani815-collab <mkhakwani815@gmail.com>
2026-08-25 10:32:16 -0700

    Django part 3 to be continued

--- Week-3/DAY_5/djangotutorial/polls/views.py
+++ Week-3/DAY_5/djangotutorial/polls/views.py
@@ -1,18 +1,24 @@
 from django.http import HttpResponse
+from . models import Question
+from django.shortcuts import render
 
+
+# def index(request):
+#     return HttpResponse("<h1>Hello Paaji.</h1>")
+
+# def success(request):
+#     return HttpResponse("<h1>Success Page</h1>")
+
+# def detail(request, question_id):
+#     return HttpResponse(f"your are looking at Question {question_id}")
+
+# def results(requests, question_id):
+#     return HttpResponse(f"You are looking at the results of question: {question_id}")
+
+# def voting(requests, question_id):
+#     return HttpResponse(f"You are voting for question: {question_id}")
 
 def index(request):
-    return HttpResponse("<h1>Hello Paaji.</h1>")
-
-def success(request):
-    return HttpResponse("<h1>Success Page</h1>")
-
-def detail(request, question_id):
-    return HttpResponse(f"your are looking at Question {question_id}")
-
-def results(requests, question_id):
-    return HttpResponse(f"You are looking at the results of question: {question_id}")
-
-def voting(requests, question_id):
-    return HttpResponse(f"You are voting for question: {question_id}")
-    
+    latest_question_list = Question.objects.order_by("pub_date")[:5]
+    context = {"latest_question_list" : latest_question_list}
+    return render(request, "polls/index.html", context)
