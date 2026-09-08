7d8d6a83d1339403946a2bf90859eaf230e1cf97
mkhakwani815-collab <mkhakwani815@gmail.com>
2026-09-08 07:19:52 -0700

    Library

--- Week-3/DAY_5/library/urls.py
+++ Week-3/DAY_5/library/urls.py
@@ -11,4 +11,3 @@ urlpatterns = [
     path('logout/', auth_views.LogoutView.as_view(next_page='book-list'), name='logout'),
     path('register/', views.register, name='register'),
 ]
-# url working
