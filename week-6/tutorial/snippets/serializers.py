26ee4f73192bae65ec094329feedb09be4c1cecb
mkhakwani815-collab <mkhakwani815@gmail.com>
2026-09-11 10:09:02 -0700

    DRF serializers + Request and Response

--- week-6/tutorial/snippets/serializers.py
+++ week-6/tutorial/snippets/serializers.py
@@ -1,30 +1,35 @@
 from rest_framework import serializers
 from snippets.models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
 
-class SnippetSerializer(serializers.Serializer):
+class SnippetSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Snippet
+        fields = ['id', 'title', 'code', 'linenos', 'language', 'style']
 
-    id =  serializers.CharField(read_only = True)
-    title = serializers.CharField(required=False, allow_blank = True,max_length = 110)
-    code = serializers.CharField(style={"base_template": "textarea.html"}) #When Rendering Html than base_template is used to render the code in textarea   
-    linenos = serializers.BooleanField(required=False)
-    language = serializers.ChoiceField(choices=LANGUAGE_CHOICES, default="python")
-    style = serializers.ChoiceField(choices=STYLE_CHOICES, default="friendly")
+# class SnippetSerializer(serializers.Serializer):
+
+#     id =  serializers.CharField(read_only = True)
+#     title = serializers.CharField(required=False, allow_blank = True,max_length = 110)
+#     code = serializers.CharField(style={"base_template": "textarea.html"}) #When Rendering Html than base_template is used to render the code in textarea   
+#     linenos = serializers.BooleanField(required=False)
+#     language = serializers.ChoiceField(choices=LANGUAGE_CHOICES, default="python")
+#     style = serializers.ChoiceField(choices=STYLE_CHOICES, default="friendly")
     
-    def create(self, validated_data):
+#     def create(self, validated_data):
         
-        """
-        Create and return a new `Snippet` instance, given the validated data.
-        """
-        return Snippet.objects.create(**validated_data)
+#         """
+#         Create and return a new `Snippet` instance, given the validated data.
+#         """
+#         return Snippet.objects.create(**validated_data)
 
-    def update(self, instance, validated_data):
-        """
-        Update and return an existing `Snippet` instance, given the validated data.
-        """
-        instance.title = validated_data.get("title", instance.title)
-        instance.code = validated_data.get("code", instance.code)
-        instance.linenos = validated_data.get("linenos", instance.linenos)
-        instance.language = validated_data.get("language", instance.language)
-        instance.style = validated_data.get("style", instance.style)
-        instance.save()
-        return instance
+#     def update(self, instance, validated_data):
+#         """
+#         Update and return an existing `Snippet` instance, given the validated data.
+#         """
+#         instance.title = validated_data.get("title", instance.title)
+#         instance.code = validated_data.get("code", instance.code)
+#         instance.linenos = validated_data.get("linenos", instance.linenos)
+#         instance.language = validated_data.get("language", instance.language)
+#         instance.style = validated_data.get("style", instance.style)
+#         instance.save()
+#         return instance
