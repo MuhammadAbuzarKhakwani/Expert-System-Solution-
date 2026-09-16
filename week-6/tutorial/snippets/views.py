4f318673a201d456e9f39f225edc25a4182a9a28
mkhakwani815-collab <mkhakwani815@gmail.com>
2026-09-16 07:04:46 -0700

    endpoint highlighted

--- week-6/tutorial/snippets/views.py
+++ week-6/tutorial/snippets/views.py
@@ -346,6 +346,16 @@ from snippets.serializers import SnippetSerializer, UserSerializer
 from snippets.permissions import IsOwnerOrReadOnly
 from rest_framework.decorators import api_view 
 from rest_framework.response import reverse 
+from rest_framework import renderers
+
+class SnippetHighlight(generics.GenericAPIView):
+    queryset = Snippet.objects.all()
+    renderer_classes = [renderers.StaticHTMLRenderer]
+
+    def get(self, request, *args, **kwargs):
+        snippet = self.get_object()
+        return Response(snippet.highlighted)
+
 
 @api_view(["GET"])
 def api_root(request,format=None)
