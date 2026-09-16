adb6c0ebced4e80936c538abaeae731b82b8f609
mkhakwani815-collab <mkhakwani815@gmail.com>
2026-09-16 07:15:26 -0700

    endpoint highlighted

--- week-6/tutorial/snippets/views.py
+++ week-6/tutorial/snippets/views.py
@@ -344,9 +344,10 @@ from rest_framework import generics, permissions
 from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer, UserSerializer
 from snippets.permissions import IsOwnerOrReadOnly
-from rest_framework.decorators import api_view 
-from rest_framework.response import reverse 
+from rest_framework.decorators import api_view  
 from rest_framework import renderers
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
 
 class SnippetHighlight(generics.GenericAPIView):
     queryset = Snippet.objects.all()
@@ -358,14 +359,13 @@ class SnippetHighlight(generics.GenericAPIView):
 
 
 @api_view(["GET"])
-def api_root(request,format=None)
+def api_root(request, format=None):
     return Response(
         {
-            "users":reverse("user-list",request=request,format = format)
-            "snippets":reverse("snippet-list",request=request,format=format)
+            "users": reverse("user-list", request=request, format=format),   # ← comma
+            "snippets": reverse("snippet-list", request=request, format=format),
         }
     )
-
 
 class SnippetList(generics.ListCreateAPIView):
     queryset = Snippet.objects.all()
