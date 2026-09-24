1cd1d74d54d4b44cfa1505da3ff435d504b6bf22
mkhakwani815-collab <mkhakwani815@gmail.com>
2026-09-24 06:23:46 -0700

    done

---
+++ week_8/mobile_inventory_ms/pos/urls.py
@@ -0,0 +1,25 @@
+from django.urls import path,include
+from rest_framework.routers import DefaultRouter
+
+from .views import (
+    CategoryViewSet,
+    BrandViewSet,
+    ProductViewSet,
+    SupplierViewSet,
+    CustomerViewSet,
+    WarehouseViewSet,
+    StockViewSet,
+)
+
+router = DefaultRouter()
+
+router.register("categories",CategoryViewSet)
+router.register("brands",BrandViewSet)
+router.register("products",ProductViewSet)
+router.register("suppliers",SupplierViewSet)
+router.register("warehouses",WarehouseViewSet)
+router.register("stocks",StockViewSet)
+
+urlpatterns = [
+    path("", include(router.urls)),
+]
--- week_8/mobile_inventory_ms/pos/views.py
+++ week_8/mobile_inventory_ms/pos/views.py
@@ -1,4 +1,4 @@
-from rest_framework import ModelViewSets
+from rest_framework.viewsets import ModelViewSet
 from .models import (
     Category,
     Brand,
@@ -26,7 +26,23 @@ class BrandViewSet(ModelViewSet):
     queryset = Brand.objects.all()
     serializer_class = BrandSerializer
 
-class ProductViewSet(ModelSerializer):
+class ProductViewSet(ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+
+class SupplierViewSet(ModelViewSet):
+    queryset = Supplier.objects.all()
+    serializer_class = SupplierSerializer
+    
+class CustomerViewSet(ModelViewSet):
+    queryset = Customer.objects.all()
+    serializer_class = CustomerSerializer
+
+class WarehouseViewSet(ModelViewSet):
+    queryset = Warehouse.objects.all()
+    serializer_class = WarehouseSerializer
+    
+class StockViewSet(ModelViewSet):
+    queryset = Stock.objects.all()
+    serializer_class = StockSerializer
 
